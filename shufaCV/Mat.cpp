ebd5291f018f00f5513234d368ecbee4fc7ea5ba
SunTY <scarsty@gmail.com>
2021-10-21 15:23:23 +0800

    add mat (#10)

--- shufaCV/Mat.cpp
+++ shufaCV/Mat.cpp
@@ -1,1 +1,20 @@
 #include "Mat.h"
+
+namespace sfcv
+{
+Mat::Mat(const std::vector<int>& dim)
+{
+    dim_ = dim;
+    data_size_ = 1;
+    for (auto d : dim_)
+    {
+        data_size_ *= d;
+    }
+    if (data_size_ > 0)
+    {
+        shared_data_ = std::make_shared<char>(data_size_);
+        data_ = shared_data_.get();
+    }
+}
+}
+
