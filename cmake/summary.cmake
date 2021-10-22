532aa8526b7528e16eebee67c699aab4d55ce667
shiquanyu <crashdown@foxmail.com>
2021-10-22 11:36:06 +0800

    fix typo

--- cmake/summary.cmake
+++ cmake/summary.cmake
@@ -2,10 +2,10 @@
 
 message(STATUS "")
 message(STATUS "")
-message(STATUS "Infomation Summary:")
+message(STATUS "Information Summary:")
 message(STATUS "")
-# CMake infomation
-message(STATUS "CMake infomation:")
+# CMake information
+message(STATUS "CMake information:")
 message(STATUS "  - CMake version:              ${CMAKE_VERSION}")
 message(STATUS "  - CMake generator:            ${CMAKE_GENERATOR}")
 message(STATUS "  - CMake building tools:       ${CMAKE_BUILD_TOOL}")
@@ -13,8 +13,8 @@ message(STATUS "  - Target System:              ${CMAKE_SYSTEM_NAME}")
 message(STATUS "")
 
 
-# C/C++ Compilier infomation
-message(STATUS "${PROJECT_NAME} toolchain infomation:")
+# C/C++ Compilier information
+message(STATUS "${PROJECT_NAME} toolchain information:")
 message(STATUS "  Cross compiling: ${CMAKE_CROSSCOMPILING}")
 message(STATUS "  C/C++ compilier:")
 message(STATUS "    - C   standard version:     C${CMAKE_C_STANDARD}")
@@ -31,23 +31,23 @@ message(STATUS "  C/C++ compilier flags:")
 message(STATUS "    - C   compilier flags:      ${CMAKE_C_FLAGS}")
 message(STATUS "    - C++ compilier flags:      ${CMAKE_CXX_FLAGS}")
 message(STATUS "  OpenMP:")
-IF (OpenMP_FOUND)
+if(OpenMP_FOUND)
 message(STATUS "    - OpenMP was found:         YES")
 message(STATUS "    - OpenMP version:           ${OpenMP_C_VERSION}")
-ELSE()
+else()
 message(STATUS "    - OpenMP was found:         NO")
-ENDIF()
+endif()
 message(STATUS "")
 
 
-# CMake project infomation
-message(STATUS "${PROJECT_NAME} building infomation:")
+# CMake project information
+message(STATUS "${PROJECT_NAME} building information:")
 message(STATUS "  - Project source path is:     ${PROJECT_SOURCE_DIR}")
 message(STATUS "  - Project building path is:   ${CMAKE_BINARY_DIR}")
 message(STATUS "")
 
 
-message(STATUS "${PROJECT_NAME} other infomation:")
+message(STATUS "${PROJECT_NAME} other information:")
 # show building install path
 message(STATUS "  Package install path:         ${CMAKE_INSTALL_PREFIX}")
 message(STATUS "")
