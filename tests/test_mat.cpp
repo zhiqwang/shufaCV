41adf918a8eeaedc99f4f8acf3aee3097741d565
Zhuo Zhang <imzhuo@foxmail.com>
2021-10-29 19:33:10 +0800

    refactor: link gtest_main, avoid writing main() for each test (#42)
    * refactor: link gtest_main, avoid writing main() for each test
    
    * try to fix CI build
    
    * fix: cmake fix for gtest_main

--- tests/test_mat.cpp
+++ tests/test_mat.cpp
@@ -43,16 +43,3 @@ TEST(Mat, pixel_value_conformance)
     data[0] = -1;
     EXPECT_EQ(data[0], -1);
 }
-
-int main(int argc, char* argv[])
-{
-    testing::InitGoogleTest(&argc, argv);
-
-    // Run a specific test only
-    //testing::GTEST_FLAG(filter) = "OpenCV.read_image";
-
-    // Exclude a specific test
-    //testing::GTEST_FLAG(filter) = "-cvtColorTwoPlane.yuv420sp_to_rgb:-cvtColorTwoPlane.rgb_to_yuv420sp"; // The writing test is broken, so skip it
-
-    return RUN_ALL_TESTS();
-}
