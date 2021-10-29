58f0e11824e877a990c873b905d22e7757f9a42b
Zhuo Zhang <imzhuo@foxmail.com>
2021-10-29 17:57:58 +0800

    refactor: link gtest_main, avoid writing main() for each test

--- tests/test_version.cpp
+++ tests/test_version.cpp
@@ -8,12 +8,3 @@ TEST(version, macros)
     EXPECT_TRUE(SHUFACV_VERSION_PATCH >= 0);
 }
 
-
-
-
-int main(int argc, char* argv[])
-{
-    testing::InitGoogleTest(&argc, argv);
-
-    return RUN_ALL_TESTS();
-}
