40035db56a1e665170bbcd26e1b35498532126d2
Zhuo Zhang <imzhuo@foxmail.com>
2021-10-31 11:35:10 +0800

    feat: Mat ctor support initializer list

--- tests/test_mat.cpp
+++ tests/test_mat.cpp
@@ -6,15 +6,23 @@ TEST(Mat, create)
     constexpr int rows = 3;
     constexpr int cols = 5;
 
+    // Mat::Mat(int, int)
     sfcv::Mat mat(rows, cols);
     EXPECT_TRUE(mat.data() != nullptr);
     EXPECT_EQ(mat.row(), rows);
     EXPECT_EQ(mat.col(), cols);
 
+    // Mat::Mat(const std::vector<int>&)
     sfcv::Mat mat2(std::vector<int>{rows, cols});
-    EXPECT_TRUE(mat.data() != nullptr);
+    EXPECT_TRUE(mat2.data() != nullptr);
     EXPECT_EQ(mat2.row(), rows);
     EXPECT_EQ(mat2.col(), cols);
+
+    // Mat::Mat(std::initializer_list<int>)
+    sfcv::Mat mat3({rows, cols});
+    EXPECT_TRUE(mat3.data() != nullptr);
+    EXPECT_EQ(mat3.row(), rows);
+    EXPECT_EQ(mat3.col(), cols);
 }
 
 TEST(Mat, create_const)
