c50e0905207c1fc50e894a04d43c11d7852130ab
Zhiqiang Wang <zhiqwang@outlook.com>
2021-11-01 21:18:46 +0800

    Update to .clang-format (#45)
    * Fixing linter
    
    * Update to .clang-format
    
    * Fixing GH Actions
    
    * Apply code-format changes
    
    * Fixing linter
    
    Co-authored-by: zhiqwang <zhiqwang@users.noreply.github.com>

--- shufaCV/Mat.cpp
+++ shufaCV/Mat.cpp
@@ -1,32 +1,30 @@
 #include "Mat.h"
 
-namespace sfcv
-{
-Mat::Mat(const std::vector<int>& dim):
-    data_size_(1), dim_(dim)
+namespace sfcv {
+Mat::Mat(const std::vector<int>& dim)
+    : data_size_(1), dim_(dim)
 {
     create();
 }
 
-Mat::Mat(const std::initializer_list<int> dim):
-    data_size_(1), dim_(dim)
+Mat::Mat(const std::initializer_list<int> dim)
+    : data_size_(1), dim_(dim)
 {
     create();
 }
 
 void Mat::create()
 {
-    for(const auto d : dim_)
+    for (const auto d : dim_)
     {
         data_size_ *= d;
     }
     if (data_size_ > 0)
     {
-        shared_data_ = std::make_shared<std::vector<char>>();
+        shared_data_ = std::make_shared<std::vector<char> >();
         shared_data_->resize(data_size_);
         data_ = shared_data_->data();
     }
 }
 
-}
-
+} // namespace sfcv
